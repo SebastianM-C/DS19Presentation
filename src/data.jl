5add80f7987933f6f6f6f80822ce4aa9253d3f21
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2019-05-22 09:39:33 -0600

    Update figshare link

--- src/data.jl
+++ src/data.jl
@@ -4,13 +4,13 @@ register(DataDep(
 	Dataset: DS19 presentation materials
 	Website: https://figshare.com/articles/DS19_presentation_materials/8146106
 	Author: Sebastian Micluța-Câmpeanu
-	Date of Publication: 2019-05-17T22:19:05Z
+	Date of Publication: 2019-05-22T15:36:08Z
 	License: MIT (https://opensource.org/licenses/MIT)
 
-	Here are the materials for the SIAM DS19 presentation entitled "Large-Scale Numerical Investigations into the Dynamics of Nonlinear Classical Systems". The dataset used for all the computations is included as the binary file graph.jls and it requires julia 1.1.0 and the two julia packages linked below to be correctly read. See the DS19Presentation GitHub repository linked below for more details on how to reproduce the presented figures.
+	Here are the materials for the SIAM DS19 presentation entitled "Large-Scale Numerical Investigations into the Dynamics of Nonlinear Classical Systems". The dataset used for all the computations is included as the binary file graph.jls and it requires julia 1.1.0 and the two julia packages linked bellow to be correctly read. See the DS19Presentation GitHub repository linked bellow for more details on how to reproduce the presented figures.
 	
 	Please cite this dataset: Micluța-Câmpeanu, Sebastian (2019): DS19 presentation materials. figshare. Conference contribution.
 	""",
-	Any["https://ndownloader.figshare.com/files/15181136"],
-	[(md5, "cefed4ae0c85586a8a31305d63da31d4")]
+	Any["https://ndownloader.figshare.com/files/15181136", "https://ndownloader.figshare.com/files/15221714"],
+	[(md5, "cefed4ae0c85586a8a31305d63da31d4"), (md5, "193df57d9f1c46aed3d8521e0b7d0028")]
 ))
